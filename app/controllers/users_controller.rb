af6b94aaeb4af72827f9ce586d92b371d553414a
aika.kayamori <kay.indig029@gmail.com>
2020-08-28 09:41:58 +0000

    修正6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
-
   def show
     @user = User.find(params[:id])
     @places = @user.places.order(id: :desc).page(params[:page])
@@ -26,6 +25,13 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  def correct_user
+    @user = current_user.users.find_by(id: params[:id])
+    unless @user
+      redirect_to root_url
+    end
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'users#show'
+  root to: 'sessions#new'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  post '/places/:id/gone', to: 'places#gone'
-  resources :users, only: [:show, :new, :create]
+  
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :places, only: [:show, :new, :create, :destroy] do
     member do
